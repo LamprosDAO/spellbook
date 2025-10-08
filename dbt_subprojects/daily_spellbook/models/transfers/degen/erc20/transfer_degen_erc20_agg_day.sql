3ec00325c327912a10a343aa086711f92be8d135
PatelPrinci <principatel@lampros.tech>
2025-10-08 15:11:11 +0530

    Degen Chain added to ERC20 transfer

---
+++ dbt_subprojects/daily_spellbook/models/transfers/degen/erc20/transfer_degen_erc20_agg_day.sql
@@ -0,0 +1,28 @@
+{{ config(
+
+        alias = 'erc20_agg_day',
+        materialized ='incremental',
+        partition_by = ['block_month'],
+        file_format ='delta',
+        incremental_strategy='merge',
+        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.block_day')],
+        unique_key = ['block_day', 'wallet_address', 'token_address']
+        )
+}}
+
+select
+    tr.blockchain,
+    date_trunc('day', tr.block_time) as block_day,
+    block_month,
+    tr.wallet_address,
+    tr.token_address,
+    cast(null as varchar) as symbol,
+    sum(tr.amount_raw) as amount_raw,
+    cast(null as double) as amount
+FROM
+{{ ref('transfers_degen_erc20') }} tr
+{% if is_incremental() %}
+-- this filter will only be applied on an incremental run
+WHERE {{ incremental_predicate('tr.block_time') }}
+{% endif %}
+GROUP BY 1, 2, 3, 4, 5
